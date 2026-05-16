e7258bb5cbbd7e17a9d600485dd223efa2f1b74e
Miguel-0611 <miguelangelgomezramirez4@gmail.com>
2026-05-16 12:51:07 -0600

    ciclo for

---
+++ Ciclo for.py
@@ -0,0 +1,13 @@
+for i in range(1, -12, -1):
+    print(i)
+def bienvenido():
+    print ("bienvenido a la clase de python")
+
+def  sumar(a, b):
+     return a+b
+
+bienvenido()
+a = float(input("ingrese el primer numero: "))       
+b = float(input("ingrese el segundo numero: "))  
+resultado = sumar(5, b)
+print("El resultado de la suma es: ", resultado)
